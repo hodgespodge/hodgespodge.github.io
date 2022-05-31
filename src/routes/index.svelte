4b2ca465cbd36aff781d9aae97fdf39815d1ea2f
sshodge2 <sshodge2@ncsu.edu>
2022-05-31 17:30:04 -0400

    added bonzi

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<svelte:head>
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script src="//cdn.rawgit.com/smore-inc/clippy.js/master/build/clippy.min.js"></script>
+
+</svelte:head>
+
+<script>
+  
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        console.log('onMount');
+        clippy.load('Bonzi', function(agent){
+            // do anything with the loaded agent
+            agent.show();
+        });
+    });
+
+
+
+</script>
