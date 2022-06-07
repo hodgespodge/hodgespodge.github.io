12f81da462e6814a3402d79649992107bace4dc0
Samuel <sshodge2@ncsu.edu>
2022-06-07 17:27:22 -0400

    got header working reasonably well

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,39 +6,54 @@
   $: currentPage = $page.url.pathname;
 </script>
 
-<Headroom duration="150ms" offset={50} tolerance={5}>
+<!-- create a svelte header using Headroom -->
+
+<!-- <header> -->
+  <Headroom duration="150ms" offset={50} tolerance={5}>
+
+    <div class="headerColor">
+    
+    <header>
+      <nav>
+        <a class="logo" href="/">
+          <h1>{site.title}</h1>
+        </a>
+        <ul>
+          {#each site.menus as { url, name }}
+            <li>
+              <a
+                sveltekit:prefetch={url.match("http") ? null : true}
+                class:active={url !== "/"
+                  ? currentPage.match(url)
+                  : url === currentPage}
+                href={url}>{name}</a
+              >
+            </li>
+          {/each}
+        </ul>
+      </nav>
+    </header>
+  </div>
+  </Headroom>
   
-  <header>
-    <nav>
-      <a class="logo" href="/">
-        <h1>{site.title}</h1>
-      </a>
-      <ul>
-        {#each site.menus as { url, name }}
-          <li>
-            <a
-              sveltekit:prefetch={url.match("http") ? null : true}
-              class:active={url !== "/"
-                ? currentPage.match(url)
-                : url === currentPage}
-              href={url}>{name}</a
-            >
-          </li>
-        {/each}
-      </ul>
-    </nav>
-  </header>
-</Headroom>
+<!-- </header> -->
 
 <style>
 
+  .headerColor {
+    background-color: #34B3F1;
+  }
+
 
   header {
-    margin-bottom: 1.5rem;
-    padding: 1rem 0;
-    border-bottom: 1px solid #ddd;
+    max-width: 48rem;
+    margin: 0 auto;
+    padding: 0 1rem;
+
+    height: 10;
 
   }
+
   nav,
   ul {
     display: flex;
@@ -53,17 +68,22 @@
     list-style: none;
   }
   h1 {
-    font-size: 2.25rem;
+    font-size: 2.5rem;
     font-weight: bold;
     margin: 0;
+    color: #000000;
     transition-duration: var(--duration-100);
   }
   h1:hover,
   a.active {
-    color: var(--brand-color);
+    /* color: var(--brand-color); */
+    color: #F15412;
+    
   }
   a {
     text-decoration: none;
+    font-size: 100%;
+    color: #000000;
   }
 
   @media screen and (max-width: 600px) {
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,20 +3,30 @@
   import "../../static/atom-dark-syntax.css";
   import "../../static/global.css";
   import Header from "$lib/components/Header.svelte";
+  import '$lib/styles/style.css';
 </script>
 
-<div class="wrapper">
-  <Header />
+<Header />
+<div class="padding"></div>
+<div class="bodywrapper">
   <main>
     <slot />
   </main>
 </div>
 
+
+
+
 <style>
-  .wrapper {
+
+  .padding {
+    padding-bottom: 4rem;
+  }
+  .bodywrapper {
     max-width: 48rem;
     margin: 0 auto;
     padding: 0 1rem;
   }
+
   /* @media screen and (max-width: 600px) {} */
 </style>
