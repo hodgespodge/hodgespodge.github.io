2e319fe3c5668543038ce4a3e2e5acb1b59cca4e
Samuel <sshodge2@ncsu.edu>
2022-10-02 14:31:50 -0400

    added next/previous page buttons to pdf component

--- src/lib/components/PDF.svelte
+++ src/lib/components/PDF.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 
-  import workerUrl from 'pdfjs-dist/build/pdf.worker.min.js?url';
-  import { setWorkerSrc } from 'svelte-pdfjs';
-  import { Button } from '@svelteuidev/core';
-  import FileSaver from "file-saver";
-  setWorkerSrc(workerUrl);
+	import workerUrl from 'pdfjs-dist/build/pdf.worker.min.js?url';
+	import { setWorkerSrc } from 'svelte-pdfjs';
+	import { Button } from '@svelteuidev/core';
+	import FileSaver from "file-saver";
+	setWorkerSrc(workerUrl);
 
 	import { base } from '$app/paths';
 
@@ -20,18 +20,50 @@
 
 	let sizing = 1;
 
-  const download_pdf = (fileURL) => {
-    let fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1);
-    FileSaver.saveAs(fileURL, fileName);
-  };
+	const download_pdf = (fileURL) => {
+		let fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1);
+		FileSaver.saveAs(fileURL, fileName);
+	};
+
+	function next_page(){
+		num += 1;
+	};
+
+	function previous_page(){
+		num -= 1;
+	}
+
+//   }
 
 </script>
 
 <section class="settings style=padding-bottom:1rem">
 <div style="width:100%; display:flex; justify-content: space-between;">
-  <div style="display:flex;">
-    Page <input type="number" bind:value={num} step="1" min="1" max={max_pages} /> of {max_pages}
-  </div>
+	<div style="display:flex;">
+	Page <input type="number" bind:value={num} step="1" min="1" max={max_pages} /> of {max_pages}
+	</div>
+
+  	<div style="display:inline-block">
+	
+		<div  style="display:inline-block;width:130px;max-width:130px">
+			{#if num > 1}
+				<Button class='previous-page' color="violet" on:click={previous_page}>
+					previous page
+				</Button> 
+			{/if}
+		</div>
+
+		<div  style="display:inline-block;width:130px;max-width:130px">
+			{#if num < max_pages}
+				<Button class='next-page' color="violet" on:click={next_page}>
+					next page
+				</Button> 
+			{/if}
+		</div>
+
+	</div>
+
+
     <Button class='download-button' variant='gradient' on:click={()=>download_pdf(filename)}>
       Download
     </Button> 
--- src/routes/blog/secondpost.md
+++ src/routes/blog/secondpost.md
@@ -2,7 +2,7 @@
 layout: post
 title: "Robot Behavior Tree Coverage"
 slug: "robot-coverage"
-description: "Placeholder"
+description: "This is a preprint copy of my research paper on test coverage for ROS2 behavior trees."
 tags:
   [
     { name: "Svelte Kit", link: "https://kit.svelte.dev/" },
